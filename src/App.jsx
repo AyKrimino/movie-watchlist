e348210657f6777e470a6461de0c6be4ce18357f
AyKrimino <ayoub.krimi14@gmail.com>
2024-03-30 23:46:44 +0100

    Adding the FavoriteMoviesSection component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import MovieListings from "./components/MovieListings";
+import FavoriteMoviesSection from "./components/FavoriteMoviesSection";
 
 const App = () => {
   return (
     <>
       <Navbar />
+      <FavoriteMoviesSection />
       <MovieListings />
       <Footer />
     </>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import logo from "../assets/images/logo.png";
 const Navbar = () => {
   return (
     <div className="bg-zinc-700 h-20 flex items-center justify-between">
-      <div className="ml-4">
+      <div className="ml-4 sm:ml-12">
         <a href="#" className="flex items-center justify-center">
           <img className="h-10 w-auto" src={logo} alt="movie watchlist logo" />
           <span className="ml-2 text-blue-400 hover:text-blue-500 md:text-lg sm:text-base text-sm font-bold">
@@ -11,7 +11,7 @@ const Navbar = () => {
           </span>
         </a>
       </div>
-      <div className="text-gray-50 hover:text-gray-100 md:text-base sm:text-sm text-xs font-bold flex items-center justify-center">
+      <div className="text-gray-50 sm:mr-10 hover:text-gray-100 md:text-base sm:text-sm text-xs font-bold flex items-center justify-center">
         <a href="#" className="mr-3 text-blue-400">
           Home
         </a>
