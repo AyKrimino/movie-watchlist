ee45bcc098d8ae7432521294926c2a2572bc13be
AyKrimino <ayoub.krimi14@gmail.com>
2024-03-31 13:28:10 +0100

    adding NavLink instead of Link component in the Navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,14 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import logo from "../assets/images/logo.png";
 
 const Navbar = () => {
+  const linkClass = ({ isActive }) =>
+    isActive ? "mr-3 text-blue-400" : "mr-3 hover:text-blue-40";
+
   return (
     <div className="bg-zinc-700 h-20 flex items-center justify-between">
       <div className="ml-4 sm:ml-12">
-        <Link to="/" className="flex items-center justify-center">
+        <NavLink to="/" className="flex items-center justify-center">
           <img
             className="h-10 w-auto select-none"
             src={logo}
@@ -14,18 +17,18 @@ const Navbar = () => {
           <span className="ml-2 text-blue-400 hover:text-blue-500 md:text-lg sm:text-base text-sm font-bold">
             Movie Watchlist
           </span>
-        </Link>
+        </NavLink>
       </div>
       <div className="text-gray-50 sm:mr-10 hover:text-gray-100 md:text-base sm:text-sm text-xs font-bold flex items-center justify-center">
-        <Link to="/" className="mr-3 text-blue-400">
+        <NavLink to="/" className={linkClass}>
           Home
-        </Link>
-        <Link to="/movies" className="mr-3 hover:text-blue-400">
+        </NavLink>
+        <NavLink to="/movies" className={linkClass}>
           Movies
-        </Link>
-        <Link to="/add-movie" className="mr-2 hover:text-blue-400">
+        </NavLink>
+        <NavLink to="/add-movie" className={linkClass}>
           Add movie
-        </Link>
+        </NavLink>
       </div>
     </div>
   );
